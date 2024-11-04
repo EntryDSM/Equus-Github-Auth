3b268749ecb9f4f0e81f4addad7d600c0d3920be
강은찬 <kangeunchan080310@gmail.com>
2024-11-04 09:15:45 +0900

    init : (#1) project setting

---
+++ src/main/kotlin/entry/dsm/gitauth/equusgithubauth/EquusGithubAuthApplication.kt
@@ -0,0 +1,11 @@
+package entry.dsm.gitauth.equusgithubauth
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class EquusGithubAuthApplication
+
+fun main(args: Array<String>) {
+    runApplication<EquusGithubAuthApplication>(*args)
+}
---
+++ src/test/kotlin/entry/dsm/gitauth/equusgithubauth/EquusGithubAuthApplicationTests.kt
@@ -0,0 +1,13 @@
+package entry.dsm.gitauth.equusgithubauth
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class EquusGithubAuthApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
